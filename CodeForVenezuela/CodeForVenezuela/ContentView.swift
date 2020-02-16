f822726e27bbead7273fa42c4b1ffcc1298fcfa6
Yumibyte <ashraigosa@gmail.com>
2020-02-16 14:03:42 -0800

    CodeForVenezuela Xcode project

---
+++ CodeForVenezuela/CodeForVenezuela/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CodeForVenezuela
+//
+//  Created by Ashley Raigosa on 2/16/20.
+//  Copyright © 2020 Ashley Raigosa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
